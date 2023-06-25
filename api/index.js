2c3f714fb8105f08e6bcb8f3275eaf2b1307e41e
Blizzrard <stang05gtsil@gmail.com>
2023-06-25 17:08:23 -0600

    Users db and api finished (I THINK)

--- api/index.js
+++ api/index.js
@@ -3,6 +3,33 @@ const apiRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
 const usersRouter = require("./users");
 apiRouter.use("/users", usersRouter);
 
@@ -13,6 +40,7 @@ const cartRouter = require("./cart");
 apiRouter.use("/cart", cartRouter);
 
 const merchRouter = require("./merch");
+const { getUserById } = require("../db/users");
 apiRouter.use("/merch", merchRouter);
 
 apiRouter.use((req, res, next) => {
--- db/seedData.js
+++ db/seedData.js
@@ -1,4 +1,5 @@
 const client = require("./client");
+const { createUser } = require("./users");
 
 async function dropTables() {
   console.log("Dropping tables");
@@ -7,20 +8,26 @@ async function dropTables() {
     DROP TABLE IF EXISTS merchandise;
     DROP TABLE IF EXISTS treats;
     DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS roles;
   `);
 }
 
 async function createTables() {
   try {
     await client.query(`
+            CREATE TABLE roles(
+              id SERIAL PRIMARY KEY,
+              title VARCHAR(100) NOT NULL
+            );
             CREATE TABLE users(
                 id SERIAL PRIMARY KEY, 
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
                 first_name VARCHAR(255) NOT NULL,
                 last_name VARCHAR(255) NOT NULL,
-                email VARCHAR(255) NOT NULL,
-                address VARCHAR(255) 
+                email VARCHAR(255) UNIQUE NOT NULL,
+                address VARCHAR(255),
+                role_id INTEGER REFERENCES roles(id) NOT NULL DEFAULT 2
             );
             CREATE TABLE treats(
                 id SERIAL PRIMARY KEY,
@@ -40,41 +47,63 @@ async function createTables() {
                 items TEXT,
                 total MONEY NOT NULL
             );
+            
         `);
-    console.log("it work");
+    console.log("Successfully created tables");
   } catch (error) {}
+}
 
-  async function createInitialUsers() {
-    console.log("Starting to create users...");
-    try {
-      const usersToCreate = [
-        {
-          username: "albert",
-          password: "bertie99",
-          firstName: "albert",
-          lastName: "bert",
-        },
-        {
-          username: "sandra",
-          password: "sandra123",
-          firstName: "sandra",
-          lastName: "daisy",
-        },
-        {
-          username: "glamgal",
-          password: "glamgal123",
-          firstName: "glamorous",
-          lastName: "gal",
-        },
-      ];
-      const users = await Promise.all(usersToCreate.map(createUser));
-      console.log("Users created:");
-      console.log(users);
-      console.log("Finished creating users!");
-    } catch (error) {
-      console.error("Error creating users!");
-      throw error;
-    }
+async function createRoles() {
+  try {
+    await client.query(`
+      INSERT INTO roles VALUES (1, 'admin');
+      INSERT INTO roles VALUES (2, 'customer');
+    `);
+  } catch (error) {}
+}
+
+async function createInitialUsers() {
+  console.log("Starting to create users...");
+  try {
+    const usersToCreate = [
+      {
+        username: "albert",
+        password: "bertie99",
+        firstName: "albert",
+        lastName: "bert",
+        email: "fake@albert.com",
+      },
+      {
+        username: "sandra",
+        password: "sandra123",
+        firstName: "sandra",
+        lastName: "daisy",
+        email: "fake@sandra.com",
+      },
+      {
+        username: "glamgal",
+        password: "glamgal123",
+        firstName: "glamorous",
+        lastName: "gal",
+        email: "fake@glam.com",
+      },
+      {
+        username: "mike",
+        password: "Password123",
+        firstName: "mike",
+        lastName: "gal",
+        email: "fake@mikeg.com",
+        roleId: 1,
+      },
+    ];
+
+    const users = await Promise.all(usersToCreate.map(createUser));
+    console.log("Users created:");
+    console.log(users);
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
+    throw error;
   }
 }
 
@@ -83,6 +112,7 @@ async function rebuildDB() {
     client.connect();
     await dropTables();
     await createTables();
+    await createRoles();
     await createInitialUsers();
   } catch (error) {}
 }
