27ad36f9d4b1cc5b69fe5dc241a9464416becdf8
Blizzrard <stang05gtsil@gmail.com>
2023-07-05 21:45:33 -0600

    cart is slowly being built, need to work on async

--- api/cart.js
+++ api/cart.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const { viewCarts } = require("../db/cart");
+const { viewCarts, viewUserCart } = require("../db/cart");
+const { getMerchById } = require("../db/merch");
 require("dotenv").config();
 const cartRouter = express.Router();
 
 cartRouter.get("/", async (req, res, next) => {
   try {
-    const response = await viewCarts();
-    res.send(response);
+    const userCart = [];
+    const response = await viewUserCart(req.user.id);
+    response.map(async (row) => {
+      if (row.product_type === "treat") {
+        const newRow = await getTreatById(row.id);
+        userCart.push(newRow);
+      } else if (row.product_type === "merch") {
+        const newRow = await getMerchById(row.id);
+        userCart.push(newRow);
+      }
+      console.log(userCart);
+    });
+    if (userCart.length === response.length) {
+      res.send(userCart);
+    } else {
+      res.send("loading");
+    }
   } catch (error) {}
 });
 
