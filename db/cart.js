acd9ca1a16135d008a8f0ada8049271a7a479538
Blizzrard <stang05gtsil@gmail.com>
2023-07-05 20:22:55 -0600

    added cart and seeding with a route to view all carts for testing, remove that later

--- db/cart.js
+++ db/cart.js
@@ -1,19 +1,35 @@
 const client = require("./client");
 
-async function addToCart(userId, productType, productId, quantity) {
+async function addToCart({ userId, productType, productId, quantity }) {
+  console.log("bro why");
   try {
     const response = await client.query(
       `
         INSERT INTO cart (user_id, product_type, product_id, quantity) 
-        VALUES ($1, $2, $3, $4);
+        VALUES ($1, $2, $3, $4)
+        RETURNING *;
     `,
       [userId, productType, productId, quantity]
     );
-    console.log(response.rows[0]);
+    console.log(response.rows);
     return response.rows;
   } catch (error) {}
 }
 
+async function viewCarts() {
+  try {
+    const response = await client.query(
+      `
+            SELECT * FROM cart;
+            `
+    );
+    return response.rows;
+  } catch (error) {}
+}
+
+console.log(addToCart(4, "merch", 23, 44));
+
 module.exports = {
   addToCart,
+  viewCarts,
 };
