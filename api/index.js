7cc6743623d78a45fdf0c8c9a49d206fd583256a
Blizzrard <stang05gtsil@gmail.com>
2023-07-13 19:43:58 -0600

    cart acting up

--- api/index.js
+++ api/index.js
@@ -7,14 +7,14 @@ apiRouter.use(async (req, res, next) => {
   const prefix = "Bearer ";
   const auth = req.header("Authorization");
 
-  if (!auth) {console.log(`req.user`);
+  if (!auth) {
     next();
   } else if (auth.startsWith(prefix)) {
     const token = auth.slice(prefix.length);
 
     try {
       const { id } = jwt.verify(token, JWT_SECRET);
-      
+
       if (id) {
         req.user = await getUserById(id);
 
